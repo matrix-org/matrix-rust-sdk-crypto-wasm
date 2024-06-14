7d961494b4cb0b9b2e64d28d5e6f3e4440282700
Damir Jelić <poljar@termina.org.uk>
2024-06-14 12:17:00 +0200

    Rename the QrCodeData methods so they use camel case

--- tests/qr_code.test.js
+++ tests/qr_code.test.js
@@ -5,15 +5,15 @@ describe(QrCodeData.name, () => {
         const base64Data =
             "TUFUUklYAgPYhmhqshl7eA4wCp1KIUdIBwDXkp85qzG55RQ3AkjtawBHaHR0cHM6Ly9yZW5kZXp2b3VzLmxhYi5lbGVtZW50LmRldi9lOGRhNjM1NS01NTBiLTRhMzItYTE5My0xNjE5ZDk4MzA2Njg";
 
-        const data = QrCodeData.from_base64(base64Data);
+        const data = QrCodeData.fromBase64(base64Data);
 
-        expect(data.public_key.toBase64()).toStrictEqual("2IZoarIZe3gOMAqdSiFHSAcA15KfOasxueUUNwJI7Ws");
-        expect(data.rendezvous_url).toStrictEqual(
+        expect(data.publicKey.toBase64()).toStrictEqual("2IZoarIZe3gOMAqdSiFHSAcA15KfOasxueUUNwJI7Ws");
+        expect(data.rendezvousUrl).toStrictEqual(
             "https://rendezvous.lab.element.dev/e8da6355-550b-4a32-a193-1619d9830668",
         );
         expect(data.mode).toStrictEqual(QrCodeMode.Login);
 
-        const encoded = data.to_base64();
+        const encoded = data.toBase64();
 
         expect(base64Data).toStrictEqual(encoded);
     });
@@ -26,11 +26,11 @@ describe(QrCodeData.name, () => {
 
         const data = new QrCodeData(publicKey, rendezvousUrl);
 
-        expect(data.public_key.toBase64()).toStrictEqual("2IZoarIZe3gOMAqdSiFHSAcA15KfOasxueUUNwJI7Ws");
-        expect(data.rendezvous_url).toStrictEqual(rendezvousUrl);
+        expect(data.publicKey.toBase64()).toStrictEqual("2IZoarIZe3gOMAqdSiFHSAcA15KfOasxueUUNwJI7Ws");
+        expect(data.rendezvousUrl).toStrictEqual(rendezvousUrl);
         expect(data.mode).toStrictEqual(QrCodeMode.Login);
 
-        const encoded = data.to_base64();
+        const encoded = data.toBase64();
         expect(base64Data).toStrictEqual(encoded);
     });
 });
