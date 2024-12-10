matrix-org/matrix-rust-sdk-crypto-wasm

commit db1af39e1b416191753ea715fb858e7b3dc48605

Publish a dual CJS/ESM package with platform-specific loaders (#167)
Proper async loading of the WASM module, with custom loaders for different environments.

index.js
@@ -1,0 +1,93 @@
+// Copyright 2024 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// @ts-check
+
+/**
+ * This is the entrypoint on non-node CommonJS environments.
+ * `asyncLoad` will load the WASM module using a `fetch` call.
+ */
+
+const bindings = require("./pkg/matrix_sdk_crypto_wasm_bg.cjs");
+
+const moduleUrl = require.resolve("./pkg/matrix_sdk_crypto_wasm_bg.wasm");
+
+// We want to throw an error if the user tries to use the bindings before
+// calling `initAsync`.
+bindings.__wbg_set_wasm(
+    new Proxy(
+        {},
+        {
+            get() {
+                throw new Error(
+                    "@matrix-org/matrix-sdk-crypto-wasm was used before it was initialized. Call `initAsync` first.",
+                );
+            },
+        },
+    ),
+);
+
+/**
+ * Stores a promise of the `loadModule` call
+ * @type {Promise<void> | null}
+ */
+let modPromise = null;
+
+/**
+ * Loads the WASM module asynchronously
+ *
+ * @returns {Promise<void>}
+ */
+async function loadModule() {
+    let mod;
+    if (typeof WebAssembly.compileStreaming === "function") {
+        mod = await WebAssembly.compileStreaming(fetch(moduleUrl));
+    } else {
+        // Fallback to fetch and compile
+        const response = await fetch(moduleUrl);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch wasm module: ${moduleUrl}`);
+        }
+        const bytes = await response.arrayBuffer();
+        mod = await WebAssembly.compile(bytes);
+    }
+
+    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")}} */
+    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
+    const instance = new WebAssembly.Instance(mod, {
+        // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
+        "./matrix_sdk_crypto_wasm_bg.js": bindings,
+    });
+
+    bindings.__wbg_set_wasm(instance.exports);
+    instance.exports.__wbindgen_start();
+}
+
+/**
+ * Load the WebAssembly module in the background, if it has not already been loaded.
+ *
+ * Returns a promise which will resolve once the other methods are ready.
+ *
+ * @returns {Promise<void>}
+ */
+async function initAsync() {
+    if (!modPromise) modPromise = loadModule();
+    await modPromise;
+}
+
+module.exports = {
+    // Re-export everything from the generated javascript wrappers
+    ...bindings,
+    initAsync,
+};

index.mjs
@@ -1,0 +1,90 @@
+// Copyright 2024 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// @ts-check
+
+/**
+ * This is the entrypoint on non-node ESM environments (such as Element Web).
+ * `asyncLoad` will load the WASM module using a `fetch` call.
+ */
+
+import * as bindings from "./pkg/matrix_sdk_crypto_wasm_bg.js";
+
+const moduleUrl = new URL("./pkg/matrix_sdk_crypto_wasm_bg.wasm", import.meta.url);
+
+// We want to throw an error if the user tries to use the bindings before
+// calling `initAsync`.
+bindings.__wbg_set_wasm(
+    new Proxy(
+        {},
+        {
+            get() {
+                throw new Error(
+                    "@matrix-org/matrix-sdk-crypto-wasm was used before it was initialized. Call `initAsync` first.",
+                );
+            },
+        },
+    ),
+);
+
+/**
+ * Stores a promise of the `loadModule` call
+ * @type {Promise<void> | null}
+ */
+let modPromise = null;
+
+/**
+ * Loads the WASM module asynchronously
+ *
+ * @returns {Promise<void>}
+ */
+async function loadModule() {
+    let mod;
+    if (typeof WebAssembly.compileStreaming === "function") {
+        mod = await WebAssembly.compileStreaming(fetch(moduleUrl));
+    } else {
+        // Fallback to fetch and compile
+        const response = await fetch(moduleUrl);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch wasm module: ${moduleUrl}`);
+        }
+        const bytes = await response.arrayBuffer();
+        mod = await WebAssembly.compile(bytes);
+    }
+
+    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")}} */
+    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
+    const instance = new WebAssembly.Instance(mod, {
+        // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
+        "./matrix_sdk_crypto_wasm_bg.js": bindings,
+    });
+
+    bindings.__wbg_set_wasm(instance.exports);
+    instance.exports.__wbindgen_start();
+}
+
+/**
+ * Load the WebAssembly module in the background, if it has not already been loaded.
+ *
+ * Returns a promise which will resolve once the other methods are ready.
+ *
+ * @returns {Promise<void>}
+ */
+export async function initAsync() {
+    if (!modPromise) modPromise = loadModule();
+    await modPromise;
+}
+
+// Re-export everything from the generated javascript wrappers
+export * from "./pkg/matrix_sdk_crypto_wasm_bg.js";

node.js
@@ -1,0 +1,118 @@
+// Copyright 2024 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// @ts-check
+
+/**
+ * This is the entrypoint on node-compatible CommonJS environments.
+ * `asyncLoad` will use `fs.readFile` to load the WASM module.
+ */
+
+const { readFileSync } = require("node:fs");
+const { readFile } = require("node:fs/promises");
+const path = require("node:path");
+const bindings = require("./pkg/matrix_sdk_crypto_wasm_bg.cjs");
+
+const filename = path.join(__dirname, "pkg/matrix_sdk_crypto_wasm_bg.wasm");
+
+// In node environments, we want to automatically load the WASM module
+// synchronously if the consumer did not call `initAsync`. To do so, we install
+// a `Proxy` that will intercept calls to the WASM module.
+bindings.__wbg_set_wasm(
+    new Proxy(
+        {},
+        {
+            get(_target, prop) {
+                const mod = loadModuleSync();
+                return initInstance(mod)[prop];
+            },
+        },
+    ),
+);
+
+/**
+ * Stores a promise which resolves to the WebAssembly module
+ * @type {Promise<WebAssembly.Module> | null}
+ */
+let modPromise = null;
+
+/**
+ * Tracks whether the module has been instantiated or not
+ * @type {boolean}
+ */
+let initialised = false;
+
+/**
+ * Loads the WASM module synchronously
+ *
+ * It will throw if there is an attempt to laod the module asynchronously running
+ *
+ * @returns {WebAssembly.Module}
+ */
+function loadModuleSync() {
+    if (modPromise) throw new Error("The WASM module is being loadded asynchronously but hasn't finished");
+    const bytes = readFileSync(filename);
+    return new WebAssembly.Module(bytes);
+}
+
+/**
+ * Loads the WASM module asynchronously
+ *
+ * @returns {Promise<WebAssembly.Module>}
+ */
+async function loadModule() {
+    const bytes = await readFile(filename);
+    return await WebAssembly.compile(bytes);
+}
+
+/**
+ * Initializes the WASM module and returns the exports from the WASM module.
+ *
+ * @param {WebAssembly.Module} mod
+ * @returns {typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")}
+ */
+function initInstance(mod) {
+    if (initialised) throw new Error("initInstance called twice");
+
+    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")}} */
+    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
+    const instance = new WebAssembly.Instance(mod, {
+        // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
+        "./matrix_sdk_crypto_wasm_bg.js": bindings,
+    });
+
+    bindings.__wbg_set_wasm(instance.exports);
+    instance.exports.__wbindgen_start();
+    initialised = true;
+    return instance.exports;
+}
+
+/**
+ * Load the WebAssembly module in the background, if it has not already been loaded.
+ *
+ * Returns a promise which will resolve once the other methods are ready.
+ *
+ * @returns {Promise<void>}
+ */
+async function initAsync() {
+    if (initialised) return;
+    if (!modPromise) modPromise = loadModule().then(initInstance);
+    await modPromise;
+}
+
+module.exports = {
+    // Re-export everything from the generated javascript wrappers
+    ...bindings,
+    initAsync,
+};

node.mjs
@@ -1,0 +1,115 @@
+// Copyright 2024 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// @ts-check
+
+/**
+ * This is the entrypoint on node-compatible ESM environments.
+ * `asyncLoad` will use `fs.readFile` to load the WASM module.
+ */
+
+import { fileURLToPath } from "node:url";
+import { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import * as bindings from "./pkg/matrix_sdk_crypto_wasm_bg.js";
+
+const filename = fileURLToPath(new URL("./pkg/matrix_sdk_crypto_wasm_bg.wasm", import.meta.url));
+
+// In node environments, we want to automatically load the WASM module
+// synchronously if the consumer did not call `initAsync`. To do so, we install
+// a `Proxy` that will intercept calls to the WASM module.
+bindings.__wbg_set_wasm(
+    new Proxy(
+        {},
+        {
+            get(_target, prop) {
+                const mod = loadModuleSync();
+                return initInstance(mod)[prop];
+            },
+        },
+    ),
+);
+
+/**
+ * Stores a promise which resolves to the WebAssembly module
+ * @type {Promise<WebAssembly.Module> | null}
+ */
+let modPromise = null;
+
+/**
+ * Tracks whether the module has been instantiated or not
+ * @type {boolean}
+ */
+let initialised = false;
+
+/**
+ * Loads the WASM module synchronously
+ *
+ * It will throw if there is an attempt to laod the module asynchronously running
+ *
+ * @returns {WebAssembly.Module}
+ */
+function loadModuleSync() {
+    if (modPromise) throw new Error("The WASM module is being loadded asynchronously but hasn't finished");
+    const bytes = readFileSync(filename);
+    return new WebAssembly.Module(bytes);
+}
+
+/**
+ * Loads the WASM module asynchronously
+ *
+ * @returns {Promise<WebAssembly.Module>}
+ */
+async function loadModule() {
+    const bytes = await readFile(filename);
+    return await WebAssembly.compile(bytes);
+}
+
+/**
+ * Initializes the WASM module and returns the exports from the WASM module.
+ *
+ * @param {WebAssembly.Module} mod
+ * @returns {typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")}
+ */
+function initInstance(mod) {
+    if (initialised) throw new Error("initInstance called twice");
+
+    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")}} */
+    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
+    const instance = new WebAssembly.Instance(mod, {
+        // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
+        "./matrix_sdk_crypto_wasm_bg.js": bindings,
+    });
+
+    bindings.__wbg_set_wasm(instance.exports);
+    instance.exports.__wbindgen_start();
+    initialised = true;
+    return instance.exports;
+}
+
+/**
+ * Load the WebAssembly module in the background, if it has not already been loaded.
+ *
+ * Returns a promise which will resolve once the other methods are ready.
+ *
+ * @returns {Promise<void>}
+ */
+export async function initAsync() {
+    if (initialised) return;
+    if (!modPromise) modPromise = loadModule().then(initInstance);
+    await modPromise;
+}
+
+// Re-export everything from the generated javascript wrappers
+export * from "./pkg/matrix_sdk_crypto_wasm_bg.js";

index.d.ts
@@ -1,0 +1,24 @@
+// Copyright 2024 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export * from "./pkg/matrix_sdk_crypto_wasm.d";
+
+/**
+ * Load the WebAssembly module in the background, if it has not already been loaded.
+ *
+ * Returns a promise which will resolve once the other methods are ready.
+ *
+ * @returns {Promise<void>}
+ */
+export function initAsync(): Promise<void>;

scripts/build.sh
@@ -1,48 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Build the JavaScript modules
 #
-# This script is really a workaround for https://github.com/rustwasm/wasm-pack/issues/1074.
-#
-# Currently, the only reliable way to load WebAssembly in all the JS
-# environments we want to target (web-via-webpack, web-via-browserify, jest)
-# seems to be to pack the WASM into base64, and then unpack it and instantiate
-# it at runtime.
-#
-# Hopefully one day, https://github.com/rustwasm/wasm-pack/issues/1074 will be
-# fixed and this will be unnecessary.
 
-set -e
+set -eux
 
-cd $(dirname "$0")/..
+cd "$(dirname "$0")"/..
 
-# --no-pack disables generation of a `package.json` file. Such a file is
-# useless to us (our package uses the hand-written one in the root directory),
-# and contains incorrect data (our `main` is `index.js`).
-wasm-pack build --no-pack --target nodejs --scope matrix-org --out-dir pkg --weak-refs "${WASM_PACK_ARGS[@]}"
+WASM_PACK_ARGS="${WASM_PACK_ARGS:-}"
 
-# Make sure that any existing package.json is deleted.
-[ -f pkg/package.json ] && rm pkg/package.json
+rm -rf pkg
 
-# Convert the Wasm into a JS file that exports the base64'ed Wasm.
-{
-  printf 'module.exports = `'
-  base64 < pkg/matrix_sdk_crypto_wasm_bg.wasm
-  printf '`;'
-} > pkg/matrix_sdk_crypto_wasm_bg.wasm.js
+# Generate the JavaScript bindings
+# --no-pack disables generation of a `package.json` file, as we're managing it ourselves.
+wasm-pack build --no-pack --target bundler --scope matrix-org --out-dir pkg --weak-refs "${WASM_PACK_ARGS}"
 
-# In the JavaScript:
-#  1. Strip out the lines that load the WASM, and our new epilogue.
-#  2. Remove the imports of `TextDecoder` and `TextEncoder`. We rely on the global defaults.
-#
-# We create a new file, rather than overwriting the old one, otherwise any
-# webpack-dev-server instance which happens to be watching will get upset over
-# the `require("path")`. We call the output "index.js" because we may as well.
-{
-  sed -e '/Text..coder.*= require(.util.)/d' \
-      -e '/^const path = /,$d' pkg/matrix_sdk_crypto_wasm.js
-  cat scripts/epilogue.js
-} > pkg/index.js
+# This will generate the following files in the `pkg` directory for us:
+#   - matrix_sdk_crypto_wasm.d.ts: TypeScript declarations of the bindings
+#   - matrix_sdk_crypto_wasm.js: logic to load the WASM module
+#   - matrix_sdk_crypto_wasm_bg.js: the JS <-> WASM glue
+#   - matrix_sdk_crypto_wasm_bg.wasm: the actual WASM module
+#   - matrix_sdk_crypto_wasm_bg.wasm.d.ts: types for the exports of the WASM module
 
-# also extend the typescript, and give it a name to match the JS.
-cat pkg/matrix_sdk_crypto_wasm.d.ts scripts/epilogue.d.ts > pkg/index.d.ts
+# We're not interested in the loading logic, as it doesn't work well on all platforms, so we ship our own loader.
+rm pkg/matrix_sdk_crypto_wasm.js
+
+# The JS <-> WASM glue uses ESM syntax, so we want to create a CommonJS version of it
+babel pkg/matrix_sdk_crypto_wasm_bg.js --out-dir pkg --out-file-extension .cjs --plugins @babel/plugin-transform-modules-commonjs

commit 68c75e5fbbde469ccf46564e576589c6e3152449

Use asynchronous wasm instantiation methods where possible (#174)
This fixes a problem (introduced in #167) on Chrome, which would complain about the module being too big. It also significantly simplifies the code on the browser entry points, because instantiateStreaming is widely supported.

index.js
@@ -45,32 +45,18 @@ bindings.__wbg_set_wasm(
 let modPromise = null;
 
 /**
- * Loads the WASM module asynchronously
+ * Loads and instantiates the WASM module asynchronously
  *
  * @returns {Promise<void>}
  */
-async function loadModule() {
-    let mod;
-    if (typeof WebAssembly.compileStreaming === "function") {
-        mod = await WebAssembly.compileStreaming(fetch(moduleUrl));
-    } else {
-        // Fallback to fetch and compile
-        const response = await fetch(moduleUrl);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch wasm module: ${moduleUrl}`);
-        }
-        const bytes = await response.arrayBuffer();
-        mod = await WebAssembly.compile(bytes);
-    }
-
-    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d.ts")}} */
-    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
-    const instance = new WebAssembly.Instance(mod, {
+async function loadModuleAsync() {
+    const { instance } = await WebAssembly.instantiateStreaming(fetch(moduleUrl), {
         // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
         "./matrix_sdk_crypto_wasm_bg.js": bindings,
     });
 
     bindings.__wbg_set_wasm(instance.exports);
+    // @ts-expect-error: Typescript doesn't know what the module exports are
     instance.exports.__wbindgen_start();
 }
 
@@ -82,7 +68,7 @@ async function loadModule() {
  * @returns {Promise<void>}
  */
 async function initAsync() {
-    if (!modPromise) modPromise = loadModule();
+    if (!modPromise) modPromise = loadModuleAsync();
     await modPromise;
 }
 

index.mjs
@@ -45,32 +45,18 @@ bindings.__wbg_set_wasm(
 let modPromise = null;
 
 /**
- * Loads the WASM module asynchronously
+ * Loads and instantiates the WASM module asynchronously
  *
  * @returns {Promise<void>}
  */
-async function loadModule() {
-    let mod;
-    if (typeof WebAssembly.compileStreaming === "function") {
-        mod = await WebAssembly.compileStreaming(fetch(moduleUrl));
-    } else {
-        // Fallback to fetch and compile
-        const response = await fetch(moduleUrl);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch wasm module: ${moduleUrl}`);
-        }
-        const bytes = await response.arrayBuffer();
-        mod = await WebAssembly.compile(bytes);
-    }
-
-    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d.ts")}} */
-    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
-    const instance = new WebAssembly.Instance(mod, {
+async function loadModuleAsync() {
+    const { instance } = await WebAssembly.instantiateStreaming(fetch(moduleUrl), {
         // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
         "./matrix_sdk_crypto_wasm_bg.js": bindings,
     });
 
     bindings.__wbg_set_wasm(instance.exports);
+    // @ts-expect-error: Typescript doesn't know what the module exports are
     instance.exports.__wbindgen_start();
 }
 
@@ -82,7 +68,7 @@ async function loadModule() {
  * @returns {Promise<void>}
  */
 export async function initAsync() {
-    if (!modPromise) modPromise = loadModule();
+    if (!modPromise) modPromise = loadModuleAsync();
     await modPromise;
 }
 

node.js
@@ -34,9 +34,9 @@ bindings.__wbg_set_wasm(
         {},
         {
             get(_target, prop) {
-                const mod = loadModuleSync();
-                // @ts-expect-error: This results to an `any` type, which is fine
-                return initInstance(mod)[prop];
+                const instance = loadModuleSync();
+                // @ts-expect-error: This results in an `any` type, which is fine
+                return instance[prop];
             },
         },
     ),
@@ -55,48 +55,57 @@ let modPromise = null;
 let initialised = false;
 
 /**
- * Loads the WASM module synchronously
+ * Loads and instantiates the WASM module synchronously
  *
- * It will throw if there is an attempt to laod the module asynchronously running
+ * It will throw if there is an attempt to load the module asynchronously running
  *
- * @returns {WebAssembly.Module}
- */
-function loadModuleSync() {
-    if (modPromise) throw new Error("The WASM module is being loadded asynchronously but hasn't finished");
-    const bytes = readFileSync(filename);
-    return new WebAssembly.Module(bytes);
-}
-
-/**
- * Loads the WASM module asynchronously
- *
- * @returns {Promise<WebAssembly.Module>}
- */
-async function loadModule() {
-    const bytes = await readFile(filename);
-    return await WebAssembly.compile(bytes);
-}
-
-/**
- * Initializes the WASM module and returns the exports from the WASM module.
- *
- * @param {WebAssembly.Module} mod
  * @returns {typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")}
  */
-function initInstance(mod) {
-    if (initialised) throw new Error("initInstance called twice");
+function loadModuleSync() {
+    if (modPromise) throw new Error("The WASM module is being loaded asynchronously but hasn't finished");
+    const bytes = readFileSync(filename);
+    const mod = new WebAssembly.Module(bytes);
 
-    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d.ts")}} */
-    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
     const instance = new WebAssembly.Instance(mod, {
         // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
         "./matrix_sdk_crypto_wasm_bg.js": bindings,
     });
 
+    initInstance(instance);
+
+    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
+    return instance.exports;
+}
+
+/**
+ * Loads and instantiates the WASM module asynchronously
+ *
+ * @returns {Promise<typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d")>}
+ */
+async function loadModuleAsync() {
+    const bytes = await readFile(filename);
+    const { instance } = await WebAssembly.instantiate(bytes, {
+        // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
+        "./matrix_sdk_crypto_wasm_bg.js": bindings,
+    });
+
+    initInstance(instance);
+
+    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
+    return instance.exports;
+}
+
+/**
+ * Initializes the WASM module and returns the exports from the WASM module.
+ *
+ * @param {WebAssembly.Instance} instance
+ */
+function initInstance(instance) {
+    if (initialised) throw new Error("initInstance called twice");
     bindings.__wbg_set_wasm(instance.exports);
+    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
     instance.exports.__wbindgen_start();
     initialised = true;
-    return instance.exports;
 }
 
 /**
@@ -108,7 +117,7 @@ function initInstance(mod) {
  */
 async function initAsync() {
     if (initialised) return;
-    if (!modPromise) modPromise = loadModule().then(initInstance);
+    if (!modPromise) modPromise = loadModuleAsync();
     await modPromise;
 }
 

node.mjs
@@ -34,9 +34,9 @@ bindings.__wbg_set_wasm(
         {},
         {
             get(_target, prop) {
-                const mod = loadModuleSync();
-                // @ts-expect-error: This results to an `any` type, which is fine
-                return initInstance(mod)[prop];
+                const instance = loadModuleSync();
+                // @ts-expect-error: This results in an `any` type, which is fine
+                return instance[prop];
             },
         },
     ),
@@ -55,48 +55,57 @@ let modPromise = null;
 let initialised = false;
 
 /**
- * Loads the WASM module synchronously
+ * Loads and instantiates the WASM module synchronously
  *
- * It will throw if there is an attempt to laod the module asynchronously running
+ * It will throw if there is an attempt to load the module asynchronously running
  *
- * @returns {WebAssembly.Module}
- */
-function loadModuleSync() {
-    if (modPromise) throw new Error("The WASM module is being loadded asynchronously but hasn't finished");
-    const bytes = readFileSync(filename);
-    return new WebAssembly.Module(bytes);
-}
-
-/**
- * Loads the WASM module asynchronously
- *
- * @returns {Promise<WebAssembly.Module>}
- */
-async function loadModule() {
-    const bytes = await readFile(filename);
-    return await WebAssembly.compile(bytes);
-}
-
-/**
- * Initializes the WASM module and returns the exports from the WASM module.
- *
- * @param {WebAssembly.Module} mod
  * @returns {typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d.ts")}
  */
-function initInstance(mod) {
-    if (initialised) throw new Error("initInstance called twice");
+function loadModuleSync() {
+    if (modPromise) throw new Error("The WASM module is being loaded asynchronously but hasn't finished");
+    const bytes = readFileSync(filename);
+    const mod = new WebAssembly.Module(bytes);
 
-    /** @type {{exports: typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d.ts")}} */
-    // @ts-expect-error: Typescript doesn't know what the instance exports exactly
     const instance = new WebAssembly.Instance(mod, {
         // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
         "./matrix_sdk_crypto_wasm_bg.js": bindings,
     });
 
+    initInstance(instance);
+
+    // @ts-expect-error: Typescript doesn't know what the module exports are
+    return instance.exports;
+}
+
+/**
+ * Loads and instantiates the WASM module asynchronously
+ *
+ * @returns {Promise<typeof import("./pkg/matrix_sdk_crypto_wasm_bg.wasm.d.ts")>}
+ */
+async function loadModuleAsync() {
+    const bytes = await readFile(filename);
+    const { instance } = await WebAssembly.instantiate(bytes, {
+        // @ts-expect-error: The bindings don't exactly match the 'ExportValue' type
+        "./matrix_sdk_crypto_wasm_bg.js": bindings,
+    });
+
+    initInstance(instance);
+
+    // @ts-expect-error: Typescript doesn't know what the module exports are
+    return instance.exports;
+}
+
+/**
+ * Initializes the WASM module and returns the exports from the WASM module.
+ *
+ * @param {WebAssembly.Instance} instance
+ */
+function initInstance(instance) {
+    if (initialised) throw new Error("initInstance called twice");
     bindings.__wbg_set_wasm(instance.exports);
+    // @ts-expect-error: Typescript doesn't know what the module exports are
     instance.exports.__wbindgen_start();
     initialised = true;
-    return instance.exports;
 }
 
 /**
@@ -108,7 +117,7 @@ function initInstance(mod) {
  */
 export async function initAsync() {
     if (initialised) return;
-    if (!modPromise) modPromise = loadModule().then(initInstance);
+    if (!modPromise) modPromise = loadModuleAsync();
     await modPromise;
 }
 